6c40f72a5741c7cf3f92e84fedac928eee1c506e
Kasper Nielsen <kasperni@gmail.com>
2013-07-31 09:41:34 +0200

    rename exporter to raw

--- ais-store-raw/src/main/java/dk/dma/ais/store/old/exporter2/RowProcessor.java
+++ ais-store-raw/src/main/java/dk/dma/ais/store/old/exporter2/RowProcessor.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-package dk.dma.ais.store.exporter2;
+package dk.dma.ais.store.old.exporter2;
 
 import org.apache.cassandra.db.columniterator.OnDiskAtomIterator;
 
