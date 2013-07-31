5d92f53312ee92c1418d6a72766f319c0aeeffcd
Kasper Nielsen <kasperni@gmail.com>
2013-07-31 13:57:58 +0200

    renaming

--- ais-store-cli/src/main/java/dk/dma/ais/store/Archiver.java
+++ ais-store-cli/src/main/java/dk/dma/ais/store/Archiver.java
@@ -26,7 +26,7 @@ import com.beust.jcommander.Parameter;
 import com.google.inject.Injector;
 
 import dk.dma.ais.packet.AisPacket;
-import dk.dma.ais.packet.AisPackets;
+import dk.dma.ais.packet.AisPacketOutputStreamSinks;
 import dk.dma.ais.reader.AisReaderGroup;
 import dk.dma.ais.store.write.DefaultAisStoreWriter;
 import dk.dma.commons.app.AbstractDaemon;
@@ -87,7 +87,7 @@ public class Archiver extends AbstractDaemon {
 
         // Starts the backup service that will write files to disk if disconnected
         final MessageToFileService<AisPacket> backupService = start(MessageToFileService.dateTimeService(
-                backup.toPath(), BACKUP_FORMAT, AisPackets.OUTPUT_TO_TEXT));
+                backup.toPath(), BACKUP_FORMAT, AisPacketOutputStreamSinks.OUTPUT_TO_TEXT));
 
         // setup an AisReader for each source
         AisReaderGroup g = AisReaderGroup.create(sources);
