a51317ef7e78e30ed655384091fa143bd7a09b9c
Jens Tuxen <mail@jenstuxen.com>
2014-07-11 16:58:49 +0200

    AISSTORE-43 patch to allow overwrite of source tag (had to move from AisPacketReader to AisReader

--- ais-store-cli/src/main/java/dk/dma/ais/store/FileImport.java
+++ ais-store-cli/src/main/java/dk/dma/ais/store/FileImport.java
@@ -17,10 +17,12 @@ package dk.dma.ais.store;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,23 +31,26 @@ import com.beust.jcommander.Parameter;
 import com.google.inject.Injector;
 
 import dk.dma.ais.packet.AisPacket;
-import dk.dma.ais.packet.AisPacketReader;
+import dk.dma.ais.reader.AisReader;
+import dk.dma.ais.reader.AisReaders;
 import dk.dma.ais.store.write.DefaultAisStoreWriter;
 import dk.dma.commons.app.AbstractCommandLineTool;
 import dk.dma.commons.service.AbstractBatchedStage;
 import dk.dma.db.cassandra.CassandraConnection;
+import dk.dma.enav.util.function.Consumer;
 
 /**
  * 
  * @author Kasper Nielsen
+ * @author Jens Tuxen
  */
-class FileImport extends AbstractCommandLineTool {
+public class FileImport extends AbstractCommandLineTool {
 
     /** The logger. */
     static final Logger LOG = LoggerFactory.getLogger(FileImport.class);
 
     @Parameter(names = "-batchSize", description = "The number of messages to write to cassandra at a time")
-    int batchSize = 1000;
+    int batchSize = 3000;
 
     @Parameter(required = true, description = "files to import...")
     List<String> sources;
@@ -58,6 +63,9 @@ class FileImport extends AbstractCommandLineTool {
 
     @Parameter(names = "-database", description = "A list of cassandra hosts that can store the data")
     List<String> cassandraSeeds = Arrays.asList("localhost");
+    
+    @Parameter(names = "-tag", description = "Overwrite the tag")
+    String tag = null;
 
     /** {@inheritDoc} */
     @Override
@@ -74,21 +82,57 @@ class FileImport extends AbstractCommandLineTool {
         start(cassandra);
 
         Set<Path> paths = new HashSet<>();
+        ArrayList<AisReader> readers = new ArrayList<>();
         for (String s : sources) {
             Path path = Paths.get(s);
             if (paths.add(path)) {
-                int count = 0;
+                final AtomicInteger count = new AtomicInteger();
                 LOG.info("Starting processing file " + path);
-                try (AisPacketReader apis = AisPacketReader.createFromFile(path, true)) {
-                    AisPacket p;
-                    while ((p = apis.readPacket()) != null) {
-                        count++;
-                        cassandra.getInputQueue().put(p);
+                try {
+                    AisReader apis = AisReaders.createReaderFromFile(path.toAbsolutePath().toString()); 
+                    if (tag != null) {
+                        apis.setSourceId(tag);
                     }
+                    
+                    apis.registerPacketHandler(new Consumer<AisPacket>() {
+
+                        @Override
+                        public void accept(AisPacket p) {
+                            try {
+                                while(!cassandra.getInputQueue().offer(p)) {
+                                    Thread.sleep(2);
+                                }
+                                
+                                count.incrementAndGet();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            
+                        }
+                    });
+                    readers.add(apis);
+                    apis.start();
+                    apis.join();
+                    
+                    
+                } finally {
+                    
                 }
+                
                 LOG.info("Finished processing file, " + count + " packets was imported from " + path);
+                
+                
             }
         }
+        
+        //one at a time ladies.
+        /*
+        for (AisReader r: readers) {
+            r.start();
+            r.join();
+        }
+        */
+        
     }
 
     public static void main(String[] args) throws Exception {
